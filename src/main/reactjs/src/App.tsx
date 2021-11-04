9c95905f483496a03de0b2fe2a31744728048e20
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2021-11-04 14:22:46 +0200

    OPHAKTKEH-23 Add new packages and update components

--- src/main/reactjs/src/App.tsx
+++ src/main/reactjs/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { AppRouter } from './routers/AppRouter';
 
 import './styles/styles.scss';
 
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  return <AppRouter />;
 };
---
+++ src/main/reactjs/src/pages/HomePage.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Button } from '@mui/material';
+
+export const HomePage: FC = () => (
+  <Button variant="contained">Hello World</Button>
+);
