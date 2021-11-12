32f85fbfb0682f964fd84993387cbbad9ba64835
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-11-12 14:37:51 +0200

    Introduce components for displaying with font sizes and weights adjusted according to style guide

--- src/main/reactjs/src/configs/materialUI.ts
+++ src/main/reactjs/src/configs/materialUI.ts
@@ -3,6 +3,11 @@ import { createTheme } from '@mui/material/styles';
 // Create Material UI theme configs
 const primaryColor = '#0a789c';
 const secondaryColor = '#ecc5c5';
+const headingColor = '#2a2a2a';
+
+const fontWeightBold = 700;
+const fontWeightMedium = 500;
+const fontWeightRegular = 400;
 
 export const theme = createTheme({
   palette: {
@@ -11,6 +16,32 @@ export const theme = createTheme({
     },
     secondary: {
       main: secondaryColor,
+    },
+  },
+  typography: {
+    htmlFontSize: 10,
+    h1: {
+      fontSize: '2.8rem',
+      fontWeight: fontWeightMedium,
+      lineHeight: '3.3rem',
+      color: headingColor,
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: fontWeightMedium,
+      lineHeight: '2.4rem',
+      color: headingColor,
+    },
+    h3: {
+      fontSize: '1.6rem',
+      fontWeight: fontWeightBold,
+      lineHeight: '1.9rem',
+      color: headingColor,
+    },
+    body1: {
+      fontSize: '1.6rem',
+      fontWeight: fontWeightRegular,
+      lineHeight: '2.4rem',
     },
   },
 });
