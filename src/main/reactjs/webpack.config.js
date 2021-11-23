ee1e3ce1bcf7329d79915baa1fc691055065b465
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-11-23 17:05:59 +0200

    Setup development time proxy to transparently route API requests from frontend to backend.

--- src/main/reactjs/webpack.config.js
+++ src/main/reactjs/webpack.config.js
@@ -71,6 +71,9 @@ module.exports = (env) => {
       },
       compress: true,
       port: 4000,
+      proxy: {
+        '/akt/api': 'http://localhost:8080'
+      },
     },
     stats: 'errors-warnings',
   };
