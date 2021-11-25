cdb250824a9d759affa91db1e2a07df69653606b
Tero Väntänen <tero.vantanen@vincit.fi>
2021-11-25 16:37:02 +0200

    OPHYKIKEH-84 Remove /akt from controllers

--- src/main/java/fi/oph/akt/api/admin/AdminTranslatorController.java
+++ src/main/java/fi/oph/akt/api/admin/AdminTranslatorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/akt/api/v1/admin/translator", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/admin/translator", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminTranslatorController {
 
 	@Resource
