e9c3d35556723cc8f7d441676031d34738631969
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-11-26 09:55:36 +0200

    Export types for root store state and dispatch.

--- src/main/reactjs/src/configs/redux.ts
+++ src/main/reactjs/src/configs/redux.ts
@@ -1,3 +1,8 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
 import configureStore from 'store/configureStore';
 
 export const store = configureStore();
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
