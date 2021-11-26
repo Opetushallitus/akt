acc921479813e819388567be3354a535218ad915
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2021-11-26 15:52:19 +0200

    OPHYKIKEH-84 Fix formatting and upgrade webpack

--- src/main/java/fi/oph/akt/config/MvcConfig.java
+++ src/main/java/fi/oph/akt/config/MvcConfig.java
@@ -11,4 +11,5 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:static/static/");
 	}
+
 }
