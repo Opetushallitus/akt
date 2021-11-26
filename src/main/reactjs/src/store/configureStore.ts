b40c03e6041409e271461151adffc8820709e39b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-11-26 09:57:42 +0200

    Hook up Saga middleware to the store

--- src/main/reactjs/src/store/configureStore.ts
+++ src/main/reactjs/src/store/configureStore.ts
@@ -1,12 +1,29 @@
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
 
+import { translatorDetailsReducer } from 'reducers/translators/translatorDetails';
+import rootSaga from 'sagas';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export default () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewareEnhancer = applyMiddleware(sagaMiddleware);
+  //  FIXME Use only in dev env and add type guards
+  const devToolsEnhancer =
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+
+  const composedEnhancers = compose(middlewareEnhancer, devToolsEnhancer);
+
   const store = createStore(
-    combineReducers({})
-    //  FIXME Use only in dev env and add type guards
-    // (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    //   (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    combineReducers({
+      translatorDetails: translatorDetailsReducer,
+    }),
+    undefined,
+    composedEnhancers
   );
 
+  sagaMiddleware.run(rootSaga);
   return store;
 };
+/* eslint-enable */
