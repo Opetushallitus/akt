c02eb1105395c96da6c7d9354ae7239edd6c266d
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2021-11-30 14:42:17 +0200

    OPHAKTKEH-76 Add redux-devtools-extension

--- src/main/reactjs/src/store/configureStore.ts
+++ src/main/reactjs/src/store/configureStore.ts
@@ -1,29 +1,21 @@
-import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 
 import { translatorDetailsReducer } from 'reducers/translators/translatorDetails';
 import rootSaga from 'sagas';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export default () => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewareEnhancer = applyMiddleware(sagaMiddleware);
-  //  FIXME Use only in dev env and add type guards
-  const devToolsEnhancer =
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__();
-
-  const composedEnhancers = compose(middlewareEnhancer, devToolsEnhancer);
 
   const store = createStore(
     combineReducers({
       translatorDetails: translatorDetailsReducer,
     }),
-    undefined,
-    composedEnhancers
+    composeWithDevTools(middlewareEnhancer)
   );
 
   sagaMiddleware.run(rootSaga);
   return store;
 };
-/* eslint-enable */
