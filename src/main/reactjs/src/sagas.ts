262a1e8030fdbf478a5ce2daa485435bfcd9b35e
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-12-01 08:38:15 +0200

    Refactor sagas for easier testing

--- src/main/reactjs/src/sagas.ts
+++ src/main/reactjs/src/sagas.ts
@@ -8,36 +8,51 @@ import {
   TranslatorDetails,
 } from 'interfaces/translator';
 
-function* getTranslatorDetails() {
+const reshapeApiResponse = (
+  details: ApiTranslatorDetails
+): TranslatorDetails => {
+  const languagePairs = details.languagePairs.map(({ fromLang, toLang }) => {
+    return { from: fromLang, to: toLang };
+  });
+  const name = details.firstName + ' ' + details.lastName;
+  return {
+    id: details.id,
+    town: details.town,
+    country: details.country,
+    name,
+    languagePairs,
+  };
+};
+
+export function* storeApiResults(
+  apiResults: AxiosResponse<PublicTranslatorListApiResponse>
+) {
+  const translatorDetails = apiResults.data.content;
+  const reshapedTranslatorDetails = translatorDetails.map(reshapeApiResponse);
+  yield put({
+    type: 'TRANSLATOR_DETAILS/RECEIVED',
+    translatorDetails: reshapedTranslatorDetails,
+  });
+}
+
+export function* getTranslatorsFromApi() {
   try {
     yield put({ type: 'TRANSLATOR_DETAILS/LOADING' });
     // TODO Add runtime validation (io-ts) for API response?
-    const apiResponse: AxiosResponse<PublicTranslatorListApiResponse> =
+    const apiResults: AxiosResponse<PublicTranslatorListApiResponse> =
       yield call(fetch, 'translator');
-    const translatorDetails = apiResponse.data.content;
-    const reshapeApiResponse = (
-      details: ApiTranslatorDetails
-    ): TranslatorDetails => {
-      const languagePairs = details.languagePairs.map(
-        ({ fromLang, toLang }) => {
-          return { from: fromLang, to: toLang };
-        }
-      );
-      const name = details.firstName + ' ' + details.lastName;
-      return { ...details, name, languagePairs };
-    };
-    const reshapedTranslatorDetails = translatorDetails.map(reshapeApiResponse);
-    yield put({
-      type: 'TRANSLATOR_DETAILS/RECEIVED',
-      translatorDetails: reshapedTranslatorDetails,
-    });
+    yield call(storeApiResults, apiResults);
   } catch (error) {
     yield put({ type: 'TRANSLATOR_DETAILS/ERROR', error });
   }
 }
 
-function* watchTranslatorDetails() {
-  yield takeLatest('TRANSLATOR_DETAILS/LOAD', getTranslatorDetails);
+export function* forceGetTranslatorsFromApi() {
+  yield call(getTranslatorsFromApi);
+}
+
+export function* watchTranslatorDetails() {
+  yield takeLatest('TRANSLATOR_DETAILS/LOAD', getTranslatorsFromApi);
 }
 
 export default function* rootSaga() {
