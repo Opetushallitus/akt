b679c2175ad7ee81bf6cef58305904de55db61e2
Tero Väntänen <tero.vantanen@vincit.fi>
2021-12-07 11:55:16 +0200

    OPHAKTKEH-116 Return ONR mock data in hard coded order

--- src/main/java/fi/oph/akt/onr/OnrServiceMock.java
+++ src/main/java/fi/oph/akt/onr/OnrServiceMock.java
@@ -1,94 +1,109 @@
 package fi.oph.akt.onr;
 
 import fi.oph.akt.onr.model.HenkiloDto;
-import fi.oph.akt.onr.model.contactDetails.*;
-import org.springframework.stereotype.Service;
-
+import fi.oph.akt.onr.model.contactDetails.ContactDetailsDto;
+import fi.oph.akt.onr.model.contactDetails.ContactDetailsGroupDto;
+import fi.oph.akt.onr.model.contactDetails.ContactDetailsGroupSource;
+import fi.oph.akt.onr.model.contactDetails.ContactDetailsGroupType;
+import fi.oph.akt.onr.model.contactDetails.YhteystietoTyyppi;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.stereotype.Service;
 
 @Service
 public class OnrServiceMock extends OnrApi {
 
 	@Override
 	public List<HenkiloDto> getHenkiloDtos(List<String> oids) {
-		return oids.stream().map(HenkiloDtoFactory::createHenkiloDto).toList();
+		final HenkiloDtoFactory henkiloDtoFactory = new HenkiloDtoFactory();
+		return oids.stream().map(henkiloDtoFactory::createHenkiloDto).toList();
 	}
 
 }
 
 class HenkiloDtoFactory {
 
-	private static final String[] menFirstNames = { "Antti", "Eero", "Ilkka", "Jari", "Juha", "Matti", "Pekka",
-			"Timo" };
+	private static Iterator<String> cyclicIter(String... values) {
+		return new CyclicIterable<String>(List.of(values)).iterator();
+	}
 
-	private static final String[] menSecondNames = { "Iiro", "Jukka", "Kalle", "Kari", "Marko", "Mikko", "Tapani",
-			"Ville" };
+	private final Iterator<String> menFirstNames = cyclicIter("Antti", "Eero", "Ilkka", "Jari", "Juha", "Matti",
+			"Pekka", "Timo");
 
-	private static final String[] womenFirstNames = { "Anneli", "Ella", "Hanna", "Iiris", "Liisa", "Maria", "Ninni",
-			"Viivi" };
+	private final Iterator<String> menSecondNames = cyclicIter("Iiro", "Jukka", "Kalle", "Kari", "Marko", "Mikko",
+			"Tapani", "Ville");
 
-	private static final String[] womenSecondNames = { "Anna", "Iida", "Kerttu", "Kristiina", "Marjatta", "Ronja",
-			"Sara" };
+	private final Iterator<String> womenFirstNames = cyclicIter("Anneli", "Ella", "Hanna", "Iiris", "Liisa", "Maria",
+			"Ninni", "Viivi");
 
-	private static final String[] surnames = { "Aaltonen", "Alanen", "Eskola", "Hakala", "Heikkinen", "Heinonen",
-			"Hiltunen", "Hirvonen", "Hämäläinen", "Kallio", "Karjalainen", "Kinnunen", "Korhonen", "Koskinen", "Laakso",
-			"Lahtinen", "Laine", "Lehtonen", "Leinonen", "Leppänen", "Manninen", "Mattila", "Mäkinen", "Nieminen",
-			"Noronen", "Ojala", "Paavola", "Pitkänen", "Räsänen", "Saarinen", "Salo", "Salonen", "Toivonen", "Tuominen",
-			"Turunen", "Valtonen", "Virtanen", "Väisänen" };
+	private final Iterator<String> womenSecondNames = cyclicIter("Anna", "Iida", "Kerttu", "Kristiina", "Marjatta",
+			"Ronja", "Sara");
 
-	private static final String[] identityNumbers = { "060105A910A", "260875-9507", "040352-904K", "130208A919P",
-			"240636-9187", "080716A957T", "120137-9646", "180720A968M", "020713A978U", "130730-960R", "151084-927A",
-			"240714A9723", "290338-944C", "280554-9389" };
+	private final Iterator<String> surnames = cyclicIter("Aaltonen", "Alanen", "Eskola", "Hakala", "Heikkinen",
+			"Heinonen", "Hiltunen", "Hirvonen", "Hämäläinen", "Kallio", "Karjalainen", "Kinnunen", "Korhonen",
+			"Koskinen", "Laakso", "Lahtinen", "Laine", "Lehtonen", "Leinonen", "Leppänen", "Manninen", "Mattila",
+			"Mäkinen", "Nieminen", "Noronen", "Ojala", "Paavola", "Pitkänen", "Räsänen", "Saarinen", "Salo", "Salonen",
+			"Toivonen", "Tuominen", "Turunen", "Valtonen", "Virtanen", "Väisänen");
 
-	private static final String[] streets = { "Malminkatu 1", "Runebergintie 2", "Sibeliuksenkuja 3", "Veturitie 4",
-			"Pirkkolantie 123" };
+	private final Iterator<String> identityNumbers = cyclicIter("060105A910A", "260875-9507", "040352-904K",
+			"130208A919P", "240636-9187", "080716A957T", "120137-9646", "180720A968M", "020713A978U", "130730-960R",
+			"151084-927A", "240714A9723", "290338-944C", "280554-9389");
 
-	private static final String[] postalCodes = { "00100", "01200", "06100", "13500", "31600", "48600", "54460" };
+	private final Iterator<String> streets = cyclicIter("Malminkatu 1", "Runebergintie 2", "Sibeliuksenkuja 3",
+			"Veturitie 4", "Pirkkolantie 123");
 
-	private static final String[] towns = { "Helsinki", "Turku", "Hämeenlinna", "Kuopio", "Lahti", "Porvoo", "Vantaa" };
+	private final Iterator<String> postalCodes = cyclicIter("00100", "01200", "06100", "13500", "31600", "48600",
+			"54460");
 
-	public static HenkiloDto createHenkiloDto(String oid) {
-		Random rand = new Random();
+	private final Iterator<String> towns = cyclicIter("Helsinki", "Turku", "Hämeenlinna", "Kuopio", "Lahti", "Porvoo",
+			"Vantaa");
+
+	private boolean genderToggle = false;
+
+	private final AtomicInteger phoneNumberCounter = new AtomicInteger();
+
+	public HenkiloDto createHenkiloDto(String oid) {
 		String nickname;
 		String secondName;
 
-		if (rand.nextBoolean()) {
-			nickname = menFirstNames[rand.nextInt(menFirstNames.length)];
-			secondName = menSecondNames[rand.nextInt(menSecondNames.length)];
+		if (genderToggle) {
+			nickname = menFirstNames.next();
+			secondName = menSecondNames.next();
 		}
 		else {
-			nickname = womenFirstNames[rand.nextInt(womenFirstNames.length)];
-			secondName = womenSecondNames[rand.nextInt(womenSecondNames.length)];
+			nickname = womenFirstNames.next();
+			secondName = womenSecondNames.next();
 		}
+		genderToggle = !genderToggle;
 
 		//@formatter:off
 		HenkiloDto henkiloDto = HenkiloDto.builder()
 				.oidHenkilo(oid)
-				.hetu(identityNumbers[rand.nextInt(identityNumbers.length)])
+				.hetu(identityNumbers.next())
 				.etunimet(nickname + " " + secondName)
 				.kutsumanimi(nickname)
-				.sukunimi(surnames[rand.nextInt(surnames.length)])
+				.sukunimi(surnames.next())
 				.build();
 		//@formatter:on
 
 		henkiloDto.setSyntymaaika(getBirthDateByIdentityNumber(henkiloDto.getHetu()));
 
-		henkiloDto
-				.setYhteystiedotRyhma(Set.of(createAktContactDetails(henkiloDto, rand), createVtjContactDetails(rand)));
+		henkiloDto.setYhteystiedotRyhma(Set.of(
+				createAktContactDetails(henkiloDto, phoneNumberCounter.incrementAndGet()), createVtjContactDetails()));
 
 		return henkiloDto;
 	}
 
-	private static ContactDetailsGroupDto createAktContactDetails(HenkiloDto henkiloDto, Random rand) {
+	private ContactDetailsGroupDto createAktContactDetails(HenkiloDto henkiloDto, int phoneCounter) {
 		ContactDetailsGroupDto detailsGroup = new ContactDetailsGroupDto();
 
 		String email = henkiloDto.getKutsumanimi().toLowerCase() + "." + henkiloDto.getSukunimi().toLowerCase()
 				+ "@akt.fi";
-		String phone = "+35840" + (1000000 + rand.nextInt(9000000));
+		String phone = "+35840" + (1000000 + phoneCounter);
 
 		Set<ContactDetailsDto> detailsSet = Set.of(
 				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_SAHKOPOSTI, email),
@@ -101,14 +116,13 @@ class HenkiloDtoFactory {
 		return detailsGroup;
 	}
 
-	private static ContactDetailsGroupDto createVtjContactDetails(Random rand) {
+	private ContactDetailsGroupDto createVtjContactDetails() {
 		ContactDetailsGroupDto detailsGroup = new ContactDetailsGroupDto();
 
 		Set<ContactDetailsDto> detailsSet = Set.of(
-				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_KATUOSOITE, streets[rand.nextInt(streets.length)]),
-				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_POSTINUMERO,
-						postalCodes[rand.nextInt(postalCodes.length)]),
-				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_KAUPUNKI, towns[rand.nextInt(towns.length)]),
+				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_KATUOSOITE, streets.next()),
+				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_POSTINUMERO, postalCodes.next()),
+				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_KAUPUNKI, towns.next()),
 				new ContactDetailsDto(YhteystietoTyyppi.YHTEYSTIETO_MAA, "Suomi"));
 
 		detailsGroup.setType(ContactDetailsGroupType.VTJ_REGULAR_DOMESTIC_ADDRESS);
@@ -118,10 +132,44 @@ class HenkiloDtoFactory {
 		return detailsGroup;
 	}
 
-	private static LocalDate getBirthDateByIdentityNumber(String identityNumber) {
+	private LocalDate getBirthDateByIdentityNumber(String identityNumber) {
 		LocalDate date = LocalDate.parse(identityNumber.substring(0, 6), DateTimeFormatter.ofPattern("ddMMyy"));
 
 		return identityNumber.charAt(6) == '-' ? date.minusYears(100) : date;
+	}
+
+}
+
+class CyclicIterable<T> implements Iterable<T> {
+
+	private final List<T> coll;
+
+	private int index = 0;
+
+	public CyclicIterable(List<T> coll) {
+		this.coll = coll;
+	}
+
+	public Iterator<T> iterator() {
+		return new Iterator<>() {
+			@Override
+			public boolean hasNext() {
+				return true;
+			}
+
+			@Override
+			public T next() {
+				if (index >= coll.size()) {
+					index = 0;
+				}
+				return coll.get(index++);
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
 	}
 
 }
