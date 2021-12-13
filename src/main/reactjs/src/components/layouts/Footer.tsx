4f696bd49e6ae36a93500eb30013c369c110cced
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2021-12-13 09:06:43 +0200

    OPHAKTKEH-46 Add footer wave svg

--- src/main/reactjs/src/components/layouts/Footer.tsx
+++ src/main/reactjs/src/components/layouts/Footer.tsx
@@ -1,55 +1,55 @@
 import { FC } from 'react';
 import { Paper, Link } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { useTranslation } from 'react-i18next';
 
 import { Text } from 'components/elements/Text';
 import { ExtLink } from 'components/elements/ExtLink';
 import { Svg } from 'components/elements/Svg';
 import Logo from 'public/assets/svg/logo.svg';
+import FooterWave from 'public/assets/svg/footer_wave.svg';
+import { useAppTranslation } from 'configs/i18n';
 
 const Footer: FC = () => {
-  const { t } = useTranslation();
+  const { t } = useAppTranslation({ keyPrefix: 'akt.component.footer' });
 
   return (
-    <Paper className="footer" elevation={3} component="footer">
-      <div className="footer__container">
-        <ExtLink
-          text="akt.component.footer.links.aktHomepage.text"
-          href="akt.component.footer.links.aktHomepage.link"
-          endIcon={<OpenInNewIcon />}
-        />
-        <ExtLink
-          text="akt.component.footer.links.accessibility.text"
-          href="akt.component.footer.links.accessibility.link"
-          endIcon={<OpenInNewIcon />}
-        />
-        <Text>{t('akt.component.footer.links.mail.text')}</Text>
-        <Link
-          href={`mailto:${t('akt.component.footer.links.mail.link')}`}
-          underline="none"
-          variant="button"
-          color="secondary"
-        >
-          {t('akt.component.footer.links.mail.link')}
-        </Link>
-        <Text>{t('akt.component.footer.links.mail.workingTime')}</Text>
-      </div>
-      <div className="footer__container">
-        <Text>{t('akt.component.footer.address.street')}</Text>
-        <Text>{t('akt.component.footer.address.zipCity')}</Text>
-        <br />
-        <Text>{t('akt.component.footer.address.phone')}</Text>
-        <Text>{t('akt.component.footer.address.fax')}</Text>
-      </div>
-      <div className="footer__container">
-        <Svg
-          className="header__left__logo"
-          src={Logo}
-          alt={t('akt.component.header.logo.alt')}
-        />
-      </div>
-    </Paper>
+    <footer>
+      <Svg src={FooterWave} alt={t('logo.alt')} />
+      <Paper className="footer" elevation={3}>
+        <div className="footer__container">
+          <ExtLink
+            text={t('links.aktHomepage.text')}
+            href={t('links.aktHomepage.link')}
+            endIcon={<OpenInNewIcon />}
+          />
+          <ExtLink
+            text={t('links.accessibility.text')}
+            href={t('links.accessibility.link')}
+            endIcon={<OpenInNewIcon />}
+          />
+          <Text>{t('links.mail.text')}</Text>
+          <Link
+            href={`mailto:${t('links.mail.link')}`}
+            underline="none"
+            variant="button"
+            color="secondary"
+          >
+            {t('links.mail.link')}
+          </Link>
+          <Text>{t('links.mail.workingTime')}</Text>
+        </div>
+        <div className="footer__container">
+          <Text>{t('address.street')}</Text>
+          <Text>{t('address.zipCity')}</Text>
+          <br />
+          <Text>{t('address.phone')}</Text>
+          <Text>{t('address.fax')}</Text>
+        </div>
+        <div className="footer__container">
+          <Svg className="header__left__logo" src={Logo} alt={t('logo.alt')} />
+        </div>
+      </Paper>
+    </footer>
   );
 };
 
