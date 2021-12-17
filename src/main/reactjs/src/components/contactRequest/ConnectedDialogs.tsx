80d6d9358d923ed3413c8ac94e3885286f8ac469
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-12-17 14:28:28 +0200

    Extract connected dialog wrappers to their own file

---
+++ src/main/reactjs/src/components/contactRequest/ConnectedDialogs.tsx
@@ -0,0 +1,70 @@
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
+import { ErrorDialog, SuccessDialog } from 'components/dialogs/Dialog';
+import { useAppTranslation } from 'configs/i18n';
+import { useAppDispatch, useAppSelector } from 'configs/redux';
+import { UIStates } from 'enums/app';
+import {
+  resetContactRequest,
+  setContactRequest,
+} from 'redux/actions/contactRequest';
+import { displayUIState } from 'redux/actions/navigation';
+import { Text } from 'components/elements/Text';
+import { ContactRequest } from 'interfaces/contactRequest';
+import { contactRequestSelector } from 'redux/selectors/contactRequest';
+
+export const SuccessDialogWrapper = () => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.contactRequestForm.successDialog',
+  });
+  const dispatch = useAppDispatch();
+  const [open, setOpen] = useState(true);
+  const cleanUp = () => {
+    dispatch(resetContactRequest);
+    dispatch(displayUIState(UIStates.PublicTranslatorListing));
+    setOpen(false);
+  };
+
+  return (
+    <SuccessDialog
+      title={t('title')}
+      content={<Text>{t('description')}</Text>}
+      actions={
+        <Button variant="contained" color="secondary" onClick={() => cleanUp()}>
+          {t('continue')}
+        </Button>
+      }
+      open={open}
+      onClose={cleanUp}
+    />
+  );
+};
+
+export const ErrorDialogWrapper = () => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.contactRequestForm.errorDialog',
+  });
+  const dispatch = useAppDispatch();
+  const request = useAppSelector(contactRequestSelector)
+    .request as ContactRequest;
+  const [open, setOpen] = useState(true);
+  const cleanUp = () => {
+    dispatch(setContactRequest(request));
+    setOpen(false);
+  };
+
+  return (
+    <ErrorDialog
+      title={t('title')}
+      content={<Text>{t('description')} akt@oph.fi</Text>}
+      actions={
+        <Button variant="contained" color="secondary" onClick={() => cleanUp()}>
+          {t('back')}
+        </Button>
+      }
+      open={open}
+      onClose={cleanUp}
+    />
+  );
+};
--- src/main/reactjs/src/components/contactRequest/ContactRequestForm.tsx
+++ src/main/reactjs/src/components/contactRequest/ContactRequestForm.tsx
@@ -37,8 +37,8 @@ import {
   selectedPublicTranslatorsForLanguagePair,
 } from 'redux/selectors/translatorDetails';
 import { contactRequestSelector } from 'redux/selectors/contactRequest';
-import { ErrorDialog, SuccessDialog } from 'components/dialogs/Dialog';
 import { ValidatedContactDetailsField } from 'components/contactRequest/ValidatedContactDetailsField';
+import { ErrorDialogWrapper, SuccessDialogWrapper } from './ConnectedDialogs';
 
 const decrementStep = (step: number) => step - 1;
 const incrementStep = (step: number) => step + 1;
@@ -272,61 +272,6 @@ const PreviewAndSendStep = () => {
         <Text>{request.message}</Text>
       </div>
     </div>
-  );
-};
-
-const SuccessDialogWrapper = () => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.component.contactRequestForm.successDialog',
-  });
-  const dispatch = useAppDispatch();
-  const [open, setOpen] = useState(true);
-  const cleanUp = () => {
-    dispatch(resetContactRequest);
-    dispatch(displayUIState(UIStates.PublicTranslatorListing));
-    setOpen(false);
-  };
-
-  return (
-    <SuccessDialog
-      title={t('title')}
-      content={<Text>{t('description')}</Text>}
-      actions={
-        <Button variant="contained" color="secondary" onClick={() => cleanUp()}>
-          {t('continue')}
-        </Button>
-      }
-      open={open}
-      onClose={cleanUp}
-    />
-  );
-};
-
-const ErrorDialogWrapper = () => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.component.contactRequestForm.errorDialog',
-  });
-  const dispatch = useAppDispatch();
-  const request = useAppSelector(contactRequestSelector)
-    .request as ContactRequest;
-  const [open, setOpen] = useState(true);
-  const cleanUp = () => {
-    dispatch(setContactRequest(request));
-    setOpen(false);
-  };
-
-  return (
-    <ErrorDialog
-      title={t('title')}
-      content={<Text>{t('description')} akt@oph.fi</Text>}
-      actions={
-        <Button variant="contained" color="secondary" onClick={() => cleanUp()}>
-          {t('back')}
-        </Button>
-      }
-      open={open}
-      onClose={cleanUp}
-    />
   );
 };
 
