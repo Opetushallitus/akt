622fd21fd12221ca77e5bdeb0a72567a370c6cf9
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2021-12-28 16:44:43 +0200

    Mock out API call to run the spec succesfully in CI

--- src/main/reactjs/src/tests/cypress/integration/public_translator_filters.spec.ts
+++ src/main/reactjs/src/tests/cypress/integration/public_translator_filters.spec.ts
@@ -1,23 +1,31 @@
+import { APIEndpoints } from 'enums/api';
 import { onPublicTranslatorFilters } from 'tests/cypress/support/page-objects/publicTranslatorFilters';
 import { onPublicTranslatorsListing } from 'tests/cypress/support/page-objects/publicTranslatorsListing';
+import { runWithIntercept } from 'tests/cypress/support/utils/api';
 
-beforeEach(() => cy.openPublicHomePage());
+beforeEach(() => {
+  runWithIntercept(
+    APIEndpoints.PublicTranslator,
+    { fixture: 'public_translators_50.json' },
+    () => cy.openPublicHomePage()
+  );
+});
 
 describe('PublicTranslatorFilters', () => {
   it('should allow filtering results by language pair, name and town', () => {
     onPublicTranslatorFilters.filterByLanguagePair('suomi', 'ruotsi');
-    onPublicTranslatorsListing.expectTranslatorsCount(2450);
-
-    onPublicTranslatorFilters.filterByName('mäkinen ii');
-    onPublicTranslatorsListing.expectTranslatorsCount(9);
+    onPublicTranslatorsListing.expectTranslatorsCount(27);
 
     onPublicTranslatorFilters.filterByTown('Helsinki');
     onPublicTranslatorsListing.expectTranslatorsCount(2);
+
+    onPublicTranslatorFilters.filterByName('aaltonen a');
+    onPublicTranslatorsListing.expectTranslatorsCount(1);
   });
 
   it('reset button should clear filters and listed translators', () => {
     onPublicTranslatorFilters.filterByLanguagePair('suomi', 'ruotsi');
-    onPublicTranslatorsListing.expectTranslatorsCount(2450);
+    onPublicTranslatorsListing.expectTranslatorsCount(27);
 
     onPublicTranslatorFilters.emptySearch();
     onPublicTranslatorsListing.expectEmptyListing();
