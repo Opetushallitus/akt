efb8e4b24779db92dbc251c6b5cfc3af5670f085
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-01-05 10:02:20 +0200

    Don't display wave on footer for clerk pages

--- src/main/reactjs/src/routers/AppRouter.tsx
+++ src/main/reactjs/src/routers/AppRouter.tsx
@@ -9,24 +9,55 @@ import { NotFoundPage } from 'pages/NotFoundPage';
 import { ClerkHomePage } from 'pages/ClerkHomePage';
 import { AppRoutes } from 'enums/app';
 
+const PublicRoutesContainer = ({ children }: { children: JSX.Element }) => (
+  <>
+    <main className="content">
+      <div className="content__container">{children}</div>
+    </main>
+    <Footer showWave={true} />
+  </>
+);
+
+const ClerkRoutesContainer = ({ children }: { children: JSX.Element }) => (
+  <>
+    <main className="content__clerk">
+      <div className="content__container">{children}</div>
+    </main>
+    <Footer showWave={false} />
+  </>
+);
+
 export const AppRouter: FC = () => (
   <BrowserRouter>
     <div className="app">
       <Header />
-      <main className="content">
-        <div className="content__container">
-          <Notifier />
-          <Routes>
-            <Route
-              path={AppRoutes.PublicHomePage}
-              element={<PublicHomePage />}
-            />
-            <Route path={AppRoutes.ClerkHomePage} element={<ClerkHomePage />} />
-            <Route path={AppRoutes.NotFoundPage} element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </main>
-      <Footer />
+      <Notifier />
+      <Routes>
+        <Route
+          path={AppRoutes.PublicHomePage}
+          element={
+            <PublicRoutesContainer>
+              <PublicHomePage />
+            </PublicRoutesContainer>
+          }
+        />
+        <Route
+          path={AppRoutes.ClerkHomePage}
+          element={
+            <ClerkRoutesContainer>
+              <ClerkHomePage />
+            </ClerkRoutesContainer>
+          }
+        />
+        <Route
+          path={AppRoutes.NotFoundPage}
+          element={
+            <PublicRoutesContainer>
+              <NotFoundPage />
+            </PublicRoutesContainer>
+          }
+        />
+      </Routes>
     </div>
   </BrowserRouter>
 );
