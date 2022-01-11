123c949a0fe102c9351bb0d10d971f6fc8cdab66
Tero Väntänen <tero.vantanen@vincit.fi>
2022-01-11 10:24:55 +0200

    Fix init sql

--- db/4_init.sql
+++ db/4_init.sql
@@ -21,16 +21,16 @@ SELECT 'id' || i::text,
            WHEN 0 THEN 'Latvia'
            ELSE country[mod(i, array_length(country, 1)) + 1] END
 FROM generate_series(1, 4900) AS i,
-     (SELECT ('{"Antti", "Eero", "Ilkka", "Jari", "Juha", "Matti", "Pekka", "Timo", "Iiro", "Jukka", "Kalle",' ||
+     (SELECT ('{"Antti", "Eero", "Ilkka", "Jari", "Juha", "Matti", "Pekka", "Timo", "Iiro", "Jukka", "Kalle", ' ||
               '"Kari", "Marko", "Mikko", "Tapani", "Ville","Anneli", "Ella", "Hanna", "Iiris", "Liisa", ' ||
               '"Maria", "Ninni", "Viivi", "Anna", "Iida", "Kerttu", "Kristiina", "Marjatta", "Ronja", ' ||
-              '"Sara"}')::text[] AS first_names) AS first_names_table,
+              '"Sara"}')::text[] AS first_names) AS first_name_table,
      (SELECT ('{"Aaltonen", "Alanen", "Eskola", "Hakala", "Heikkinen", "Heinonen", "Hiltunen", "Hirvonen", ' ||
               '"Hämäläinen", "Kallio", "Karjalainen", "Kinnunen", "Korhonen", "Koskinen", "Laakso", ' ||
               '"Lahtinen", "Laine", "Lehtonen", "Leinonen", "Leppänen", "Manninen", "Mattila", "Mäkinen", ' ||
-              '"Nieminen", "Noronen", "Ojala", "Paavola", "Pitkänen", "Räsänen", "Saarinen", "Salo",' ||
-              ' "Salonen", "Toivonen", "Tuominen", "Turunen", "Valtonen", "Virtanen", ' ||
-              '"Väisänen"}')::text[] AS last_names) AS last_names_table,
+              '"Nieminen", "Noronen", "Ojala", "Paavola", "Pitkänen", "Räsänen", "Saarinen", "Salo", ' ||
+              '"Salonen", "Toivonen", "Tuominen", "Turunen", "Valtonen", "Virtanen", ' ||
+              '"Väisänen"}')::text[] AS last_names) AS last_name_table,
      (SELECT ('{"Malminkatu 1", "Runebergintie 2", "Sibeliuksenkuja 3", "Veturitie 4", ' ||
               '"Pirkkolantie 123"}')::text[] AS street) AS street_table,
      (SELECT ('{"Helsinki", "Turku", "Hämeenlinna", "Kuopio", "Lahti", "Porvoo", "Vantaa", "Järvenpää", ' ||
@@ -39,36 +39,6 @@ FROM generate_series(1, 4900) AS i,
      (SELECT ('{"00100", "01200", "06100", "13500", "31600", "48600", ' ||
               '"54460"}')::text[] AS postal_code) AS postal_code_table,
      (SELECT ('{"Suomi", "suomi", "SUOMI", "Finland", "", NULL}')::text[] AS country) AS country_table;
-
--- some translator fields set to null
-UPDATE translator
-SET ssn=NULL
-WHERE mod(translator_id, 50) = 0;
-
-UPDATE translator
-SET email=NULL
-WHERE mod(translator_id, 51) = 0;
-
-UPDATE translator
-SET phone_number=NULL
-WHERE mod(translator_id, 52) = 0;
-
-UPDATE translator
-SET street=NULL
-WHERE mod(translator_id, 53) = 0;
-
--- NOTE mod 87 has expired term, but VIR never expires
-UPDATE translator
-SET town=NULL
-WHERE translator_id IN (SELECT t.translator_id
-                        FROM translator t
-                                 JOIN authorisation a on t.translator_id = a.translator_id
-                        WHERE mod(t.translator_id, 87) = 0
-                          and basis <> 'VIR');
-
-UPDATE translator
-SET postal_code=NULL
-WHERE mod(translator_id, 55) = 0;
 
 INSERT INTO authorisation(translator_id, basis, meeting_date_id, aut_date, kkt_check, vir_date, assurance_date) (
     SELECT translator_id,
@@ -123,6 +93,37 @@ INSERT INTO authorisation_term(authorisation_id, begin_date, end_date) (
     FROM authorisation
     WHERE meeting_date_id IS NOT NULL
 );
+
+-- set some translator fields to null
+UPDATE translator
+SET ssn=NULL
+WHERE mod(translator_id, 50) = 0;
+
+UPDATE translator
+SET email=NULL
+WHERE mod(translator_id, 51) = 0;
+
+UPDATE translator
+SET phone_number=NULL
+WHERE mod(translator_id, 52) = 0;
+
+UPDATE translator
+SET street=NULL
+WHERE mod(translator_id, 53) = 0;
+
+-- NOTE mod 87 has expired term, but VIR never expires
+UPDATE translator
+SET town=NULL
+WHERE translator_id IN (SELECT t.translator_id
+                        FROM translator t
+                                 JOIN authorisation a on t.translator_id = a.translator_id
+                        WHERE mod(t.translator_id, 87) = 0
+                          and basis <> 'VIR');
+
+UPDATE translator
+SET postal_code=NULL
+WHERE mod(translator_id, 55) = 0;
+
 -- fi <-> sv
 INSERT INTO language_pair(authorisation_id, from_lang, to_lang, permission_to_publish) (
     SELECT authorisation_id, 'FI', 'SV', true
