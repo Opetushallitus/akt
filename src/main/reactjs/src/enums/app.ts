057dac4b4cf1b35411f7df7aeddc5e4ee26a54c9
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-01-12 16:36:56 +0200

    Move clerk homepage to /akt/virkailija, remove obsolete route for ContactRequest

--- src/main/reactjs/src/enums/app.ts
+++ src/main/reactjs/src/enums/app.ts
@@ -4,7 +4,6 @@ export enum AppConstants {
 
 export enum AppRoutes {
   PublicHomePage = '/',
-  ContactRequestPage = '/yhteydenotto',
   ClerkHomePage = '/akt/virkailija',
   NotFoundPage = '*',
 }
