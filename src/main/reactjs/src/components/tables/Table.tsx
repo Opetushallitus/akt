72c5fb4e8586dfd83a2291c3a8c4b75faaba423b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-01-26 17:47:38 +0200

    Reuse extracted count variable in component

--- src/main/reactjs/src/components/tables/Table.tsx
+++ src/main/reactjs/src/components/tables/Table.tsx
@@ -62,7 +62,7 @@ export function PaginatedTable<T extends WithId>({
       <div className="table__head-box">
         <TablePagination
           className="table__head-box__pagination"
-          count={data.length}
+          count={count}
           component="div"
           onPageChange={(_event, newPage) => setPage(newPage)}
           page={page}
