bf8bdbfb346526a9b5d102e8bc54a5c869bdb250
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-01-31 10:02:09 +0200

    Refactor: move view components to another file

---
+++ src/main/reactjs/src/components/clerkTranslator/ClerkTranslatorDetails.tsx
@@ -0,0 +1,250 @@
+import {
+  Button,
+  TableCell,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+
+import { H2, Text } from 'components/elements/Text';
+import { TextBox } from 'components/elements/TextBox';
+import {
+  useAppTranslation,
+  useKoodistoLanguagesTranslation,
+} from 'configs/i18n';
+import { AppRoutes, Variant, Color } from 'enums/app';
+import { Authorisation } from 'interfaces/authorisation';
+import { ClerkTranslator } from 'interfaces/clerkTranslator';
+import { DateUtils } from 'utils/date';
+
+const TopControls = () => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.clerkTranslatorDetails',
+  });
+
+  return (
+    <div className="columns">
+      <Button
+        component={Link}
+        to={AppRoutes.ClerkHomePage}
+        id="back-btn"
+        variant={Variant.Text}
+        startIcon={<ArrowBackIcon />}
+      >
+        {t('buttons.back')}
+      </Button>
+    </div>
+  );
+};
+
+const TranslatorDetailsSection = ({
+  translator,
+}: {
+  translator: ClerkTranslator;
+}) => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.clerkTranslatorDetails',
+  });
+
+  return (
+    <>
+      <div className="columns">
+        <H2 className="grow">{t('header.personalInformation')}</H2>
+        <Button variant={Variant.Contained} color={Color.Secondary}>
+          {t('buttons.edit')}
+        </Button>
+      </div>
+      <div className="columns gapped">
+        <TextBox
+          label={t('labels.lastName')}
+          value={translator.contactDetails.lastName}
+          disabled
+        />
+        <TextBox
+          label={t('labels.firstNames')}
+          value={translator.contactDetails.firstName}
+          disabled
+        />
+        <TextBox
+          label={t('labels.ssn')}
+          value={translator.contactDetails.identityNumber}
+          disabled
+        />
+      </div>
+      <H2>{t('header.address')}</H2>
+      <div className="columns gapped">
+        <TextBox
+          label={t('labels.streetAddress')}
+          value={translator.contactDetails.street}
+          disabled
+        />
+        <TextBox
+          label={t('labels.zipCode')}
+          value={translator.contactDetails.postalCode}
+          disabled
+        />
+        <TextBox
+          label={t('labels.postOffice')}
+          value={translator.contactDetails.town}
+          disabled
+        />
+        <TextBox
+          label={t('labels.country')}
+          value={translator.contactDetails.country}
+          disabled
+        />
+      </div>
+      <H2>{t('header.contactInformation')}</H2>
+      <div className="columns gapped">
+        <TextBox
+          label={t('labels.email')}
+          value={translator.contactDetails.email}
+          disabled
+        />
+        <TextBox
+          label={t('labels.phoneNumber')}
+          value={translator.contactDetails.phoneNumber}
+          disabled
+        />
+      </div>
+    </>
+  );
+};
+
+// TODO Used also in selectors/clerkTranslator.ts - extract to utils?
+const isAuthorisationValid = (
+  { effectiveTerm }: Authorisation,
+  currentDate: Date
+) => {
+  return !(
+    effectiveTerm?.end &&
+    DateUtils.isDatePartBeforeOrEqual(effectiveTerm.end, currentDate)
+  );
+};
+
+const CenteredIconCell = ({ icon }: { icon?: JSX.Element }) => {
+  // Centering icon with inline styles since class selectors get overriden by MUIs classes
+  // and more specific selectors (IDs) can't be used as there will be multiple instances
+  // of these components in the DOM.
+  return <TableCell sx={{ textAlign: 'center' }}>{icon}</TableCell>;
+};
+
+const AuthorisationDetailsSection = ({
+  translator,
+}: {
+  translator: ClerkTranslator;
+}) => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.clerkTranslatorDetails',
+  });
+  const translateLanguage = useKoodistoLanguagesTranslation();
+  const currentDate = new Date();
+
+  return (
+    <>
+      <div className="columns">
+        <H2 className="grow">{t('header.authorisations')}</H2>
+        <Button
+          variant={Variant.Contained}
+          color={Color.Secondary}
+          startIcon={<AddIcon />}
+        >
+          {t('buttons.addAuthorisation')}
+        </Button>
+      </div>
+      <Table
+        size="small"
+        className="clerk-translator-details-page__content-container--authorisations-table"
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell>{t('authorisation.languagePair')}</TableCell>
+            <TableCell>{t('authorisation.startDate')}</TableCell>
+            <TableCell>{t('authorisation.endDate')}</TableCell>
+            <TableCell>{t('authorisation.basis')}</TableCell>
+            <TableCell>{t('authorisation.isValid')}</TableCell>
+            <TableCell>{t('authorisation.isPublished')}</TableCell>
+            <TableCell>{t('authorisation.caseId')}</TableCell>
+            <TableCell>{t('authorisation.save')}</TableCell>
+            <TableCell>{t('authorisation.delete')}</TableCell>
+            <TableCell>{t('authorisation.edit')}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {translator.authorisations.map((a, i) => (
+            <TableRow key={i}>
+              <TableCell>
+                <Text>
+                  {`${translateLanguage(a.languagePair.from)}
+                 - ${translateLanguage(a.languagePair.to)}`}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>
+                  {DateUtils.formatOptionalDate(a.effectiveTerm?.start)}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>
+                  {DateUtils.formatOptionalDate(a.effectiveTerm?.end)}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>{a.basis}</Text>
+              </TableCell>
+              <TableCell>
+                <Text>
+                  {isAuthorisationValid(a, currentDate)
+                    ? t('authorisation.yes')
+                    : t('authorisation.no')}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>
+                  {a.permissionToPublish
+                    ? t('authorisation.yes')
+                    : t('authorisation.no')}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>{i}</Text>
+              </TableCell>
+              <CenteredIconCell />
+              <CenteredIconCell />
+              <CenteredIconCell icon={<EditIcon />} />
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
+
+export const ClerkTranslatorDetails = ({
+  translator,
+}: {
+  translator: ClerkTranslator;
+}) => {
+  return (
+    <>
+      <TopControls />
+      <div className="rows gapped">
+        <TranslatorDetailsSection translator={translator} />
+        <AuthorisationDetailsSection translator={translator} />
+      </div>
+    </>
+  );
+};
+
+export const ClerkTranslatorNotFoundView = () => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.clerkTranslatorDetails',
+  });
+
+  return <H2>{t('notFound')}</H2>;
+};
--- src/main/reactjs/src/pages/ClerkTranslatorDetailsPage.tsx
+++ src/main/reactjs/src/pages/ClerkTranslatorDetailsPage.tsx
@@ -1,278 +1,26 @@
-import {
-  Box,
-  Button,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from '@mui/material';
-import { Link, useParams } from 'react-router-dom';
+import { Box, Paper } from '@mui/material';
+import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
 
-import { H1, H2, Text } from 'components/elements/Text';
-import { TextBox } from 'components/elements/TextBox';
+import { H1 } from 'components/elements/Text';
 import { ProgressIndicator } from 'components/elements/ProgressIndicator';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
-import {
-  useAppTranslation,
-  useKoodistoLanguagesTranslation,
-} from 'configs/i18n';
+import { useAppTranslation } from 'configs/i18n';
 import { APIResponseStatus } from 'enums/api';
-import { AppRoutes, Color, Severity, Variant } from 'enums/app';
-import { ClerkTranslator } from 'interfaces/clerkTranslator';
+import { Color, Severity } from 'enums/app';
 import { clerkTranslatorsSelector } from 'redux/selectors/clerkTranslator';
 import { loadClerkTranslators } from 'redux/actions/clerkTranslator';
 import { showNotifierToast } from 'redux/actions/notifier';
 import { Utils } from 'utils';
-import { DateUtils } from 'utils/date';
-import { Authorisation } from 'interfaces/authorisation';
+import {
+  ClerkTranslatorDetails,
+  ClerkTranslatorNotFoundView,
+} from 'components/clerkTranslator/ClerkTranslatorDetails';
 
 const paramAsNum = (param: string | undefined) => {
   if (param) {
     return parseInt(param);
   }
-};
-
-const TranslatorNotFoundView = () => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.pages.clerkTranslatorDetailsPage.translatorNotFoundView',
-  });
-
-  return (
-    <Paper
-      elevation={3}
-      className="clerk-translator-details-page__content-container"
-    >
-      <H2>{t('title')}</H2>
-    </Paper>
-  );
-};
-
-const TopControls = () => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.component.clerkTranslatorDetails',
-  });
-
-  return (
-    <div className="columns">
-      <Button
-        component={Link}
-        to={AppRoutes.ClerkHomePage}
-        id="back-btn"
-        variant={Variant.Text}
-        startIcon={<ArrowBackIcon />}
-      >
-        {t('buttons.back')}
-      </Button>
-    </div>
-  );
-};
-
-const TranslatorDetailsSection = ({
-  translator,
-}: {
-  translator: ClerkTranslator;
-}) => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.component.clerkTranslatorDetails',
-  });
-
-  return (
-    <>
-      <div className="columns">
-        <H2 className="grow">{t('header.personalInformation')}</H2>
-        <Button variant={Variant.Contained} color={Color.Secondary}>
-          {t('buttons.edit')}
-        </Button>
-      </div>
-      <div className="columns gapped">
-        <TextBox
-          label={t('labels.lastName')}
-          value={translator.contactDetails.lastName}
-          disabled
-        />
-        <TextBox
-          label={t('labels.firstNames')}
-          value={translator.contactDetails.firstName}
-          disabled
-        />
-        <TextBox
-          label={t('labels.ssn')}
-          value={translator.contactDetails.identityNumber}
-          disabled
-        />
-      </div>
-      <H2>{t('header.address')}</H2>
-      <div className="columns gapped">
-        <TextBox
-          label={t('labels.streetAddress')}
-          value={translator.contactDetails.street}
-          disabled
-        />
-        <TextBox
-          label={t('labels.zipCode')}
-          value={translator.contactDetails.postalCode}
-          disabled
-        />
-        <TextBox
-          label={t('labels.postOffice')}
-          value={translator.contactDetails.town}
-          disabled
-        />
-        <TextBox
-          label={t('labels.country')}
-          value={translator.contactDetails.country}
-          disabled
-        />
-      </div>
-      <H2>{t('header.contactInformation')}</H2>
-      <div className="columns gapped">
-        <TextBox
-          label={t('labels.email')}
-          value={translator.contactDetails.email}
-          disabled
-        />
-        <TextBox
-          label={t('labels.phoneNumber')}
-          value={translator.contactDetails.phoneNumber}
-          disabled
-        />
-      </div>
-    </>
-  );
-};
-
-// TODO Used also in selectors/clerkTranslator.ts - extract to utils?
-const isAuthorisationValid = (
-  { effectiveTerm }: Authorisation,
-  currentDate: Date
-) => {
-  return !(
-    effectiveTerm?.end &&
-    DateUtils.isDatePartBeforeOrEqual(effectiveTerm.end, currentDate)
-  );
-};
-
-const CenteredIconCell = ({ icon }: { icon?: JSX.Element }) => {
-  // Centering icon with inline styles since class selectors get overriden by MUIs classes
-  // and more specific selectors (IDs) can't be used as there will be multiple instances
-  // of these components in the DOM.
-  return <TableCell sx={{ textAlign: 'center' }}>{icon}</TableCell>;
-};
-
-const AuthorisationDetailsSection = ({
-  translator,
-}: {
-  translator: ClerkTranslator;
-}) => {
-  const { t } = useAppTranslation({
-    keyPrefix: 'akt.component.clerkTranslatorDetails',
-  });
-  const translateLanguage = useKoodistoLanguagesTranslation();
-  const currentDate = new Date();
-
-  return (
-    <>
-      <div className="columns">
-        <H2 className="grow">{t('header.authorisations')}</H2>
-        <Button
-          variant={Variant.Contained}
-          color={Color.Secondary}
-          startIcon={<AddIcon />}
-        >
-          {t('buttons.addAuthorisation')}
-        </Button>
-      </div>
-      <Table
-        size="small"
-        className="clerk-translator-details-page__content-container--authorisations-table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>{t('authorisation.languagePair')}</TableCell>
-            <TableCell>{t('authorisation.startDate')}</TableCell>
-            <TableCell>{t('authorisation.endDate')}</TableCell>
-            <TableCell>{t('authorisation.basis')}</TableCell>
-            <TableCell>{t('authorisation.isValid')}</TableCell>
-            <TableCell>{t('authorisation.isPublished')}</TableCell>
-            <TableCell>{t('authorisation.caseId')}</TableCell>
-            <TableCell>{t('authorisation.save')}</TableCell>
-            <TableCell>{t('authorisation.delete')}</TableCell>
-            <TableCell>{t('authorisation.edit')}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {translator.authorisations.map((a, i) => (
-            <TableRow key={i}>
-              <TableCell>
-                <Text>
-                  {`${translateLanguage(a.languagePair.from)}
-                 - ${translateLanguage(a.languagePair.to)}`}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {DateUtils.formatOptionalDate(a.effectiveTerm?.start)}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {DateUtils.formatOptionalDate(a.effectiveTerm?.end)}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{a.basis}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {isAuthorisationValid(a, currentDate)
-                    ? t('authorisation.yes')
-                    : t('authorisation.no')}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {a.permissionToPublish
-                    ? t('authorisation.yes')
-                    : t('authorisation.no')}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{i}</Text>
-              </TableCell>
-              <CenteredIconCell />
-              <CenteredIconCell />
-              <CenteredIconCell icon={<EditIcon />} />
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
-  );
-};
-
-const TranslatorDetailsView = ({
-  translator,
-}: {
-  translator: ClerkTranslator;
-}) => {
-  return (
-    <Paper
-      elevation={3}
-      className="clerk-translator-details-page__content-container"
-    >
-      <TopControls />
-      <div className="rows gapped">
-        <TranslatorDetailsSection translator={translator} />
-        <AuthorisationDetailsSection translator={translator} />
-      </div>
-    </Paper>
-  );
 };
 
 const TranslatorViewDispatcher = () => {
@@ -298,9 +46,9 @@ const TranslatorViewDispatcher = () => {
       return <div />;
     case APIResponseStatus.Success:
       if (translator) {
-        return <TranslatorDetailsView translator={translator} />;
+        return <ClerkTranslatorDetails translator={translator} />;
       } else {
-        return <TranslatorNotFoundView />;
+        return <ClerkTranslatorNotFoundView />;
       }
   }
 };
@@ -327,7 +75,12 @@ export const ClerkTranslatorDetailsPage = () => {
   return (
     <Box className="clerk-send-email-page">
       <H1>{t('pages.clerkTranslatorDetailsPage.title')}</H1>
-      <TranslatorViewDispatcher />
+      <Paper
+        elevation={3}
+        className="clerk-translator-details-page__content-container"
+      >
+        <TranslatorViewDispatcher />
+      </Paper>
     </Box>
   );
 };
