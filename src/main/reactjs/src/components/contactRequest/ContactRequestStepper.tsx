36547848bbf1bf24ef3d41b80f39af0ea0f99b13
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-02-23 15:01:23 +0200

    Add aria-labels also to stepper step labels

--- src/main/reactjs/src/components/contactRequest/ContactRequestStepper.tsx
+++ src/main/reactjs/src/components/contactRequest/ContactRequestStepper.tsx
@@ -2,8 +2,9 @@ import { Step, StepLabel, Stepper } from '@mui/material';
 
 import { stepsByIndex } from 'components/contactRequest/ContactRequestFormUtils';
 import { CircularStepper } from 'components/elements/CircularStepper';
-import { useAppTranslation } from 'configs/i18n';
+import { useAppTranslation, useCommonTranslation } from 'configs/i18n';
 import { useAppSelector } from 'configs/redux';
+import { ContactRequestFormStep } from 'enums/contactRequest';
 import { useWindowProperties } from 'hooks/useWindowProperties';
 import { contactRequestSelector } from 'redux/selectors/contactRequest';
 
@@ -13,11 +14,28 @@ export const ContactRequestStepper = () => {
   const { t } = useAppTranslation({
     keyPrefix: 'akt.component.contactRequestForm.steps',
   });
+  const translateCommon = useCommonTranslation();
+  const phasePrefix = translateCommon('phase');
 
   const maxStep = Object.keys(stepsByIndex).length;
   const currentStep = activeStep + 1;
   const text = `${currentStep}/${maxStep}`;
   const value = currentStep * (100 / maxStep);
+  const stepAriaLabel = (step: ContactRequestFormStep) => {
+    const phaseDescription = t(stepsByIndex[step]);
+    const phaseNumberPart = `${step + 1}/${maxStep}`;
+    if (step < activeStep) {
+      return `${phasePrefix} ${phaseNumberPart}, ${t(
+        'completed'
+      )}: ${phaseDescription}`;
+    } else if (step == activeStep) {
+      return `${phasePrefix} ${phaseNumberPart}, ${t(
+        'active'
+      )}: ${phaseDescription}`;
+    } else {
+      return `${phasePrefix} ${phaseNumberPart}: ${phaseDescription}`;
+    }
+  };
 
   return isPhone ? (
     <CircularStepper
@@ -31,6 +49,7 @@ export const ContactRequestStepper = () => {
       {Object.values(stepsByIndex).map((v, i) => (
         <Step key={v}>
           <StepLabel
+            aria-label={stepAriaLabel(i)}
             className={
               activeStep < i
                 ? 'contact-request-page__stepper__step--disabled'
