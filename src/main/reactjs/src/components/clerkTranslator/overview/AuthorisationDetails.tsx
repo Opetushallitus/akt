789ddebafb739fe5aa4346f94a00a1fcf14a4c58
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-03-09 09:18:18 +0200

    Extract authorisations table to its own component

--- src/main/reactjs/src/components/clerkTranslator/overview/AuthorisationDetails.tsx
+++ src/main/reactjs/src/components/clerkTranslator/overview/AuthorisationDetails.tsx
@@ -1,5 +1,6 @@
-import { Add as AddIcon, Edit as EditIcon } from '@mui/icons-material';
+import { Add as AddIcon, DeleteOutline } from '@mui/icons-material';
 import {
+  Switch,
   Table,
   TableBody,
   TableCell,
@@ -16,20 +17,92 @@ import {
 } from 'configs/i18n';
 import { useAppSelector } from 'configs/redux';
 import { Color, Variant } from 'enums/app';
+import { Authorisation } from 'interfaces/authorisation';
 import { clerkTranslatorOverviewSelector } from 'redux/selectors/clerkTranslatorOverview';
 import { AuthorisationUtils } from 'utils/authorisation';
 import { DateUtils } from 'utils/date';
+
+const AuthorisationsListing = ({
+  authorisations,
+}: {
+  authorisations: Array<Authorisation>;
+}) => {
+  const translateLanguage = useKoodistoLanguagesTranslation();
+  const translateCommon = useCommonTranslation();
+  const { t } = useAppTranslation({
+    keyPrefix: 'akt.component.clerkTranslatorOverview.authorisations',
+  });
+  const currentDate = new Date();
+
+  return (
+    <Table
+      size="small"
+      className="clerk-translator-details__authorisations-table"
+    >
+      <TableHead>
+        <TableRow>
+          <TableCell>{t('fields.languagePair')}</TableCell>
+          <TableCell>{t('fields.basis')}</TableCell>
+          <TableCell>{t('fields.startDate')}</TableCell>
+          <TableCell>{t('fields.endDate')}</TableCell>
+          <TableCell>{t('fields.isValid')}</TableCell>
+          <TableCell>{t('fields.isPublished')}</TableCell>
+          <TableCell>{t('fields.diaryNumber')}</TableCell>
+          <TableCell>{translateCommon('delete')}</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {authorisations.map((a, i) => (
+          <TableRow
+            key={i}
+            data-testid={`authorisations-table__id-${a.id}-row`}
+          >
+            <TableCell>
+              <Text>
+                {`${translateLanguage(a.languagePair.from)}
+             - ${translateLanguage(a.languagePair.to)}`}
+              </Text>
+            </TableCell>
+            <TableCell>
+              <Text>{a.basis}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{DateUtils.formatOptionalDate(a.termBeginDate)}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{DateUtils.formatOptionalDate(a.termEndDate)}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>
+                {AuthorisationUtils.isAuthorisationEffective(a, currentDate)
+                  ? translateCommon('yes')
+                  : translateCommon('no')}
+              </Text>
+            </TableCell>
+            <TableCell>
+              <Switch checked={a.permissionToPublish} color={Color.Secondary} />
+            </TableCell>
+            <TableCell>
+              <Text>{a.diaryNumber}</Text>
+            </TableCell>
+            <TableCell className="clerk-translator-details__authorisations-table--centered">
+              <DeleteOutline color={Color.Error} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
 
 export const AuthorisationDetails = () => {
   const { t } = useAppTranslation({
     keyPrefix: 'akt.component.clerkTranslatorOverview.authorisations',
   });
-  const translateLanguage = useKoodistoLanguagesTranslation();
-  const translateCommon = useCommonTranslation();
+
   const { selectedTranslator } = useAppSelector(
     clerkTranslatorOverviewSelector
   );
-  const currentDate = new Date();
 
   return (
     <>
@@ -44,71 +117,11 @@ export const AuthorisationDetails = () => {
           {t('buttons.add')}
         </CustomButton>
       </div>
-      <Table
-        size="small"
-        className="clerk-translator-overview__authorisations-table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>{t('fields.languagePair')}</TableCell>
-            <TableCell>{t('fields.basis')}</TableCell>
-            <TableCell>{t('fields.startDate')}</TableCell>
-            <TableCell>{t('fields.endDate')}</TableCell>
-            <TableCell>{t('fields.isValid')}</TableCell>
-            <TableCell>{t('fields.isPublished')}</TableCell>
-            <TableCell>{t('fields.diaryNumber')}</TableCell>
-            <TableCell>{translateCommon('save')}</TableCell>
-            <TableCell>{translateCommon('delete')}</TableCell>
-            <TableCell>{translateCommon('edit')}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {selectedTranslator?.authorisations.map((a, i) => (
-            <TableRow
-              key={i}
-              data-testid={`authorisations-table__id-${a.id}-row`}
-            >
-              <TableCell>
-                <Text>
-                  {`${translateLanguage(a.languagePair.from)}
-                 - ${translateLanguage(a.languagePair.to)}`}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{a.basis}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{DateUtils.formatOptionalDate(a.termBeginDate)}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{DateUtils.formatOptionalDate(a.termEndDate)}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {AuthorisationUtils.isAuthorisationEffective(a, currentDate)
-                    ? translateCommon('yes')
-                    : translateCommon('no')}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>
-                  {a.permissionToPublish
-                    ? translateCommon('yes')
-                    : translateCommon('no')}
-                </Text>
-              </TableCell>
-              <TableCell>
-                <Text>{a.diaryNumber}</Text>
-              </TableCell>
-              <TableCell />
-              <TableCell />
-              <TableCell className="clerk-translator-overview__authorisations-table__centered">
-                <EditIcon />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <AuthorisationsListing
+        authorisations={
+          selectedTranslator ? selectedTranslator.authorisations : []
+        }
+      />
     </>
   );
 };
