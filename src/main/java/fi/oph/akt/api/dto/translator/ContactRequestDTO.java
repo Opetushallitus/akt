ffa1a364b88ddaac1b8d6a2932b6da45469c5aac
Mika Huttunen <mika.huttunen@gofore.com>
2022-03-09 16:24:32 +0200

    OPHAKTKEH-280: no validation for email in backend

--- src/main/java/fi/oph/akt/api/dto/translator/ContactRequestDTO.java
+++ src/main/java/fi/oph/akt/api/dto/translator/ContactRequestDTO.java
@@ -1,7 +1,6 @@
 package fi.oph.akt.api.dto.translator;
 
 import java.util.List;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import lombok.Builder;
@@ -9,7 +8,7 @@ import lombok.Builder;
 public record ContactRequestDTO(
   @NotEmpty @Size(max = 255) String firstName,
   @NotEmpty @Size(max = 255) String lastName,
-  @NotEmpty @Email String email,
+  @NotEmpty @Size(max = 255) String email,
   @Size(max = 255) String phoneNumber,
   @NotEmpty @Size(max = 6000) String message,
   @NotEmpty @Size(max = 10) String fromLang,
