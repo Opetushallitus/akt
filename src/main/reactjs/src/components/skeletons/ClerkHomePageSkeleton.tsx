f475a2cb750a64aa61fd2106f8744abbe95823dc
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-03-17 09:14:50 +0200

    Review feedback: extract ClerkHomePageControlButtons to its own file

--- src/main/reactjs/src/components/skeletons/ClerkHomePageSkeleton.tsx
+++ src/main/reactjs/src/components/skeletons/ClerkHomePageSkeleton.tsx
@@ -1,3 +1,4 @@
+import { ClerkHomePageControlButtons } from 'components/clerkHomePage/ClerkHomePageControlButtons';
 import { ClerkTranslatorAutocompleteFilters } from 'components/clerkTranslator/filters/ClerkTranslatorAutocompleteFilters';
 import { ClerkTranslatorToggleFilters } from 'components/clerkTranslator/filters/ClerkTranslatorToggleFilters';
 import { ClerkTranslatorListing } from 'components/clerkTranslator/listing/ClerkTranslatorListing';
@@ -5,7 +6,6 @@ import { CustomSkeleton } from 'components/elements/CustomSkeleton';
 import { H2 } from 'components/elements/Text';
 import { useCommonTranslation } from 'configs/i18n';
 import { SkeletonVariant } from 'enums/app';
-import { ClerkHomePageControlButtons } from 'pages/clerk/ClerkHomePage';
 
 export const ClerkHomePageSkeleton = () => {
   const translateCommon = useCommonTranslation();
