41dd5f935e75dc17979b36a501218028a851ab18
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-03-22 12:39:52 +0200

    OPHAKTKEH-300 Rename CustomCircularProgress ClassName

--- src/main/reactjs/src/components/elements/CustomCircularProgress.tsx
+++ src/main/reactjs/src/components/elements/CustomCircularProgress.tsx
@@ -1,7 +1,7 @@
 import { CircularProgress, CircularProgressProps } from '@mui/material';
 
 export const CustomCircularProgress = (props: CircularProgressProps) => (
-  <div className="progress-indicator">
+  <div className="custom-circular-progress">
     <CircularProgress {...props} />
   </div>
 );
