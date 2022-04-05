6fd2d2858e5d5732f1f19558e94b394e37a00b8e
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-04-05 09:50:21 +0300

    Disable the next button independently from the error label shown on the form

--- src/main/reactjs/src/components/contactRequest/steps/WriteMessage.tsx
+++ src/main/reactjs/src/components/contactRequest/steps/WriteMessage.tsx
@@ -8,7 +8,7 @@ import {
 } from 'components/contactRequest/ContactRequestFormUtils';
 import { CustomTextField } from 'components/elements/CustomTextField';
 import { H3 } from 'components/elements/Text';
-import { useAppTranslation } from 'configs/i18n';
+import { translateOutsideComponent, useAppTranslation } from 'configs/i18n';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
 import { TextFieldTypes } from 'enums/app';
 import { ContactRequestFormStep } from 'enums/contactRequest';
@@ -18,6 +18,17 @@ import { contactRequestSelector } from 'redux/selectors/contactRequest';
 import { Utils } from 'utils';
 import { StringUtils } from 'utils/string';
 
+const getErrorForMessage = (message: string) => {
+  const t = translateOutsideComponent();
+  const error = Utils.inspectCustomTextFieldErrors(
+    TextFieldTypes.Textarea,
+    message,
+    true
+  );
+
+  return error ? t(`akt.${error}`) : '';
+};
+
 export const WriteMessage = ({
   disableNext,
 }: {
@@ -25,7 +36,7 @@ export const WriteMessage = ({
 }) => {
   // I18n
   const { t } = useAppTranslation({
-    keyPrefix: 'akt',
+    keyPrefix: 'akt.component.contactRequestForm',
   });
 
   //Windows properties
@@ -40,7 +51,11 @@ export const WriteMessage = ({
 
   useEffect(() => {
     const hasBlankMessage = StringUtils.isBlankString(request?.message);
-    const hasFieldError = fieldError.length > 0;
+    // Instead of relying on fieldError in local state, compute the error text
+    // separately to ensure that we can disable the next button independently of
+    // showing the error labels. The error label is to be shown only
+    // after an onBlur event has occurred.
+    const hasFieldError = getErrorForMessage(request?.message || '').length > 0;
 
     disableNext(hasBlankMessage || hasFieldError);
   }, [disableNext, fieldError, request]);
@@ -57,15 +72,8 @@ export const WriteMessage = ({
   const handleMessageFieldErrors = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    const { type, value, required } = event.target;
-    const error = Utils.inspectCustomTextFieldErrors(
-      type as TextFieldTypes,
-      value,
-      required
-    );
-
-    const errorMessage = error ? t(error) : '';
-    setFieldError(errorMessage);
+    const { value } = event.target;
+    setFieldError(getErrorForMessage(value));
   };
 
   const getHelperMessage = () => {
@@ -73,9 +81,7 @@ export const WriteMessage = ({
     const errorToShow = fieldError ? `${fieldError}.` : '';
     const maxLength = Utils.getMaxTextAreaLength();
 
-    return `${errorToShow} ${value?.length} / ${maxLength} ${t(
-      'component.contactRequestForm.characters'
-    )}`;
+    return `${errorToShow} ${value?.length} / ${maxLength} ${t('characters')}`;
   };
 
   return (
@@ -88,16 +94,14 @@ export const WriteMessage = ({
         <div className="rows gapped">
           <H3>
             {t(
-              `component.contactRequestForm.steps.${
+              `steps.${
                 ContactRequestFormStep[ContactRequestFormStep.WriteMessage]
               }`
             )}
           </H3>
           <CustomTextField
             id="contact-request-page__message-field"
-            label={t(
-              'component.contactRequestForm.formLabels.writeMessageHere'
-            )}
+            label={t('formLabels.writeMessageHere')}
             value={request?.message}
             type={TextFieldTypes.Textarea}
             onBlur={handleMessageFieldErrors}
